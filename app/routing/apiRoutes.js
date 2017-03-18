5e923f4afbd11851736c2958685bc71286fa35ec
Meredith Moore <meredithmoore@Merediths-MacBook-Pro.local>
2017-03-18 00:55:19 -0400

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,8 +21,62 @@ module.exports = function(app) {
 
         console.log(newUnfriend);
 
-        unfriends.push(newUnfriend);
+        // unfriends.push(newUnfriend);
 
-        res.json(newUnfriend);
+        // res.json(newUnfriend);
+
+
+
+
+
+        //holds the best match and will update as it loops through the options
+        var unfriendliest = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        //posts and parses the result of the users's survey
+        var userData = req.body;
+        var userName = newUnfriend.name;
+        var userPhoto = newUnfriend.photo;
+        var userScores = newUnfriend.scores;
+
+
+        //calculates the difference between the users's scores and the scores of each user in the database
+
+        var totalDifference = 0;
+
+        //loops through the friend possibilities in the database
+        for (var i = 0; i < unfriends.length; i++) {
+            console.log(unfriends[i].name);
+            totalDifference = 0;
+
+            //loops through all of the scores for each friend
+            for (var j = 0; j < unfriends[i].scores[j]; j++) {
+                //calculate the total difference between the scores and sums into the totalDifference
+
+                totalDifference += Math.abs(
+                    parseInt(userScores[j]) -
+                    parseInt(unfriends[i].scores[j]));
+
+                //if sum of diff is less then the differences of the current 'best match'
+                if (totalDifference <= unfriendliest.unfriendsDifference) {
+
+                    //resets the best match to the new friend
+
+                    unfriendliest.name = unfriends[i].name;
+                    unfriendliest.age = unfriends[i].age;
+                    unfriendliest.photo = unfriends[i].photo;
+
+                    unfriendliest.friendDifference = totalDifference;
+                }
+            }
+        }
+        //saves the user's data to the database 
+        unfriends.push(userData);
+
+        //return a JSON with user's best match to be used by html
+        res.json(unfriendliest);
+
     });
-}
+};
